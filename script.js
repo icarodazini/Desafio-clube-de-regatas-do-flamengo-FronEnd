73202abb6f02ef60cf3b61ce733dfb888ed48758
icarodazini <icarodazini7@gmail.com>
2022-11-26 19:22:44 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,9 +1,107 @@
-function toggle(botaoDoCoracao){
-    var btnvar1 = document.getElementById(botaoDoCoracao);
-         if (botaoDoCoracao1.style.color =="red") {
-            botaoDoCoracao1.style.color = "black"
-         }
-         else{
-             btnvar1.style.color = "red"
-         }
+function trocaCor1() {
+    var botaoDoCoracao = document.getElementById('heart1');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor2() {
+    var botaoDoCoracao = document.getElementById('heart2');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor3() {
+    var botaoDoCoracao = document.getElementById('heart3');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor4() {
+    var botaoDoCoracao = document.getElementById('heart4');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor5() {
+    var botaoDoCoracao = document.getElementById('heart5');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor6() {
+    var botaoDoCoracao = document.getElementById('heart6');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor7() {
+    var botaoDoCoracao = document.getElementById('heart7');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor8() {
+    var botaoDoCoracao = document.getElementById('heart8');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor9() {
+    var botaoDoCoracao = document.getElementById('heart9');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor10() {
+    var botaoDoCoracao = document.getElementById('heart10');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor11() {
+    var botaoDoCoracao = document.getElementById('heart11');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
+}
+
+function trocaCor12() {
+    var botaoDoCoracao = document.getElementById('heart12');
+    if (botaoDoCoracao.style.color == "red"){
+        botaoDoCoracao.style.color = "black"
+    } else {
+        botaoDoCoracao.style.color = "red"
+    }
 }
