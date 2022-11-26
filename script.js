fda1b90733ce470e145b67e64f14cdb0c324fae0
icarodazini <icarodazini7@gmail.com>
2022-11-26 18:07:15 -0300

    flamengo

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-finctin +{}
+function toggle(botaoDoCoracao){
+    var btnvar1 = document.getElementById(botaoDoCoracao);
+         if (botaoDoCoracao1.style.color =="red") {
+            botaoDoCoracao1.style.color = "black"
+         }
+         else{
+             btnvar1.style.color = "red"
+         }
+}
